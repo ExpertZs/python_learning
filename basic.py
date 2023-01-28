37e28cf45bfb707769e905ef85390fbc122eea1d
ExpertZs <zs.freelancer1@gmail.com>
2023-01-28 12:24:59 +0600

    Variable in python

--- basic.py
+++ basic.py
@@ -4,3 +4,9 @@ print('Hey, This is Md. Zahidul Islam')
 print('0----')
 print(' ||||')
 print('*' * 10)
+
+#Variables in  python
+name = 'Jhon Smith'
+age = 20
+is_newPatient = True
+print(name, age, is_newPatient)
