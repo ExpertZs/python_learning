55bccdf5e6a1f52fc7be6023c102b40f99ef7b46
ExpertZs <zs.freelancer1@gmail.com>
2023-01-28 12:40:53 +0600

    Receiving Input from user

--- basic.py
+++ basic.py
@@ -10,3 +10,11 @@ name = 'Jhon Smith'
 age = 20
 is_newPatient = True
 print(name, age, is_newPatient)
+
+#Receiving input from user
+user_name = input('What is your name? ')
+print('Hi ' + user_name)
+    #Ask two question 1. name 2. favourite color and print like - Zahid likes Blue
+person_name= input('What is your name? ')
+favourite_color = input('What is your favourite color? ')
+print(person_name + ' likes ' + favourite_color)
