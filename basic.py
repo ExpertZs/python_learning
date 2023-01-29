cb8e6ae5ebd7805bbf4d409f6a512baa9b29aabd
ExpertZs <zs.freelancer1@gmail.com>
2023-01-29 15:33:12 +0600

    Creating Reusables Function

--- basic.py
+++ basic.py
@@ -310,4 +310,21 @@ def square(number):
 result = square(6)
 print(result)
 
+#Creating reusable function example of emoji converter
+
+def emoji_converter(message):
+    words = message.split(" ")
+    output= " "
+    emojis ={
+        ":)": "😂",
+        "(:": "😢"
+    }
+    for word in words:
+        output+= emojis.get(word, word) + " "
+    return output
+
+
+message = input("> ")
+result= emoji_converter(message)
+print(result)
 
