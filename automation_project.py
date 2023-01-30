30908ddf5a2645c7317d8890c231e24d9708f2c7
ExpertZs <zs.freelancer1@gmail.com>
2023-01-30 22:51:51 +0600

    Automation spreed sheet project

--- automation_project.py
+++ automation_project.py
@@ -1,12 +1,12 @@
 import openpyxl as xl
-from openpyxl.chart import Reference, BarChart
+from openpyxl.chart import BarChart, Reference
+filename = input("file name: ")
+#output_file_name=input("ouput file name: ")
 
-filename = input("filename: ")
-print(filename)
+
 def process_workbook(filename):
-
-    wb=xl.load_workbook(filename)
-    sheet= wb["Sheet1"]
+    wb = xl.load_workbook(filename)
+    sheet = wb["Sheet1"]
 
     for row in range(2, sheet.max_row+1):
         cell=sheet.cell(row, 3)
@@ -14,13 +14,18 @@ def process_workbook(filename):
         corrected_price_cell = sheet.cell(row, 5)
         corrected_price_cell.value = corrected_price
 
-    values = Reference(sheet,
-                       min_row = 2,
-                       max_row = sheet.max_row,
-                       min_col = 5,
-                       max_col = 5
-        )
+
+    values = Reference( sheet,
+                        min_row = 2,
+                        max_row = sheet.max_row,
+                        min_col = 5,
+                        max_col = 5)
+
+
     chart = BarChart()
     chart.add_data(values)
     sheet.add_chart(chart, 'f2')
-    wb.save("transaction3.xlsx")
+    wb.save(filename)
+
+
+process_workbook(filename)
