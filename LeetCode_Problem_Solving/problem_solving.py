9608c264a32467cda56720f1cb1dd9ed558797b8
ExpertZs <zs.freelancer1@gmail.com>
2023-02-04 14:42:06 +0600

    #LeetCode Problem 217. Contains Duplicate

--- LeetCode_Problem_Solving/problem_solving.py
+++ LeetCode_Problem_Solving/problem_solving.py
@@ -42,6 +42,21 @@ class Solution:
             left_sum += nums[index]
         return -1
 
+#LeetCode Problem 217. Contains Duplicate
+#Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
 
+#Example 1:
+#Input: nums = [1,2,3,1]
+#Output: true
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        hashmap = dict()
+        for index in range(len(nums)):
+            if nums[index] in hashmap.keys():
+                return True
+            else:
+                hashmap[nums[index]] = index
+        return False
 
 
