244ce16e06bc1ce856da588c87646f4dba5f0fc5
ExpertZs <zs.freelancer1@gmail.com>
2023-02-05 19:47:50 +0600

    LeetCode Problem 1. Two Sum

--- LeetCode_Problem_Solving/problem_solving.py
+++ LeetCode_Problem_Solving/problem_solving.py
@@ -108,6 +108,22 @@ class Solution:
             pointer2 += 1
         return True if pointer1 == len(s) else False
 
+#LeetCode Problem 1. Two Sum
+#Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+#You may assume that each input would have exactly one solution, and you may not use the same element twice.
+#You can return the answer in any order.
+#Example 1:
+#Input: nums = [2,7,11,15], target = 9
+#Output: [0,1]
+#Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
 
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashMap = dict()
+        for index, number in enumerate(nums):
+            remainings = target - number
+            if remainings in hashMap:
+                return [hashMap[remainings], index]
+            hashMap[number] = index
 
 
