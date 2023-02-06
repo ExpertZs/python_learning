d8693ec5afceef4a05b1cc30493301735e4a4602
ExpertZs <zs.freelancer1@gmail.com>
2023-02-06 22:08:05 +0600

    LeetCode Problem 121. Best Time to Buy and Sell Stock

--- LeetCode_Problem_Solving/problem_solving.py
+++ LeetCode_Problem_Solving/problem_solving.py
@@ -217,3 +217,30 @@ class Solution:
                 nums2.remove(index)
         return result
 
+
+#LeetCode Problem 121. Best Time to Buy and Sell Stock
+#You are given an array prices where prices[i] is the price of a given stock on the ith day.
+#You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+#Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+#Example 1:
+#Input: prices = [7,1,5,3,6,4]
+#Output: 5
+#Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+#Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        buying_pointer, selling_pointer = 0, 1
+        max_profit = 0
+
+        while selling_pointer < len(prices):
+            if prices[buying_pointer] < prices[selling_pointer]:
+                profit = prices[selling_pointer] - prices[buying_pointer]
+                max_profit = max(max_profit, profit)
+            else:
+                buying_pointer = selling_pointer
+            selling_pointer += 1
+        return max_profit
+
+
