46e36066c3b0abe33854de7f68edf8e8b18cf1f3
ExpertZs <zs.freelancer1@gmail.com>
2023-02-07 22:49:54 +0600

    leetCode Problem 566. Reshape the Matrix

--- LeetCode_Problem_Solving/problem_solving.py
+++ LeetCode_Problem_Solving/problem_solving.py
@@ -311,3 +311,35 @@ class Solution:
             fast = fast.next
         return fast
 
+#leetCode Problem 566. Reshape the Matrix
+#In MATLAB, there is a handy function called reshape which can reshape an m x n matrix into a new one with a different size r x c keeping its original data.
+#You are given an m x n matrix mat and two integers r and c representing the number of rows and the number of columns of the wanted reshaped matrix.
+#The reshaped matrix should be filled with all the elements of the original matrix in the same row-traversing order as they were.
+#If the reshape operation with given parameters is possible and legal, output the new reshaped matrix; Otherwise, output the original matrix.
+
+#Example 1:
+#Input: mat = [[1,2],[3,4]], r = 1, c = 4
+#Output: [[1,2,3,4]]
+
+class Solution:
+    def matrixReshape(self, mat: List[List[int]], r: int, c: int) -> List[List[int]]:
+        numbers = []
+        for index1 in range(len(mat)):
+            for index2 in range(len(mat[0])):
+                numbers.append(mat[index1][index2])
+
+        if r * c > len(numbers):
+            return mat
+
+        results = []
+
+        index = 0
+        for index1 in range(r):
+            results.append([])
+            for index2 in range(c):
+                results[index1].append(numbers[index])
+                index += 1
+
+        return results
+
+
