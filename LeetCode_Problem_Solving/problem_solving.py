450bf7ba7a422d376ba2c7f113740d59777206ca
ExpertZs <zs.freelancer1@gmail.com>
2023-02-08 11:11:34 +0600

    LeetCode Problem 36. Valid Sudoku

--- LeetCode_Problem_Solving/problem_solving.py
+++ LeetCode_Problem_Solving/problem_solving.py
@@ -380,3 +380,41 @@ class Solution:
             return len(s) - len(store_dic) + 1
 
 
+#LeetCode Problem 36. Valid Sudoku
+#Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+#    Each row must contain the digits 1-9 without repetition.
+#    Each column must contain the digits 1-9 without repetition.
+#    Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+#Note:
+#    A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+#    Only the filled cells need to be validated according to the mentioned rules.
+
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        def is_valid_row():
+            for row in board:
+                if not is_valid(row):
+                    return False
+            return True
+
+        def is_valid_col():
+            for col in zip(*board):
+                if not is_valid(col):
+                    return False
+            return True
+
+        def is_valid_square():
+            for i in (0, 3, 6):
+                for j in (0, 3, 6):
+                    square = [board[x][y] for x in range(i, i + 3)
+                              for y in range(j, j + 3)]
+                    if not is_valid(square):
+                        return False
+            return True
+
+        def is_valid(value):
+            res = [i for i in value if i != "."]
+            return len(res) == len(set(res))
+
+        return is_valid_row() and is_valid_col() and is_valid_square()
+
