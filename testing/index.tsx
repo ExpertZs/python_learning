fdd1216e994d8645d7cfe1bd9ccb50970a9e2059
ExpertZs <zs.freelancer1@gmail.com>
2023-04-14 16:02:52 +0600

    testing

---
+++ testing/index.tsx
@@ -0,0 +1,11 @@
+<Box
+        position="absolute"
+        width={271}
+        height={141}
+        left={732}
+        top={240}
+        bgcolor="#295C7A"
+        boxShadow="0px 4px 4px rgba(0, 0, 0, 0.25)"
+        borderRadius={4}
+      />
+    </Box>
